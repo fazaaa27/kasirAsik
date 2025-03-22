faa01adf1ff37fb3b4b6b271bf439b63910bc95f
Ndaru <daruprasetiyo123@gmail.com>
2025-03-22 07:05:01 +0300

    feat: add feature kasir/models.py
    menambahkan model class Product, Category, Cart, Transaction dan DetailTransaction
    didalam folder kasir pada file models.py
    menambahkan model pada tampilan admin

--- backend/kasir/models.py
+++ backend/kasir/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.PositiveIntegerField()
+    stock = models.PositiveIntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+class Cart(models.Model):
+    amount = models.IntegerField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+class Transaction(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    amount = models.IntegerField()
+
+    def __str__(self):
+        return self.date
+    
+class DetailTransaction(models.Model):
+    transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+
+    def __str__(self):
+        return self.transaction
