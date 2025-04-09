f17d1a56ed237193a7fd8e4061bf9650d409bc62
Ndaru <daruprasetiyo123@gmail.com>
2025-04-09 20:33:55 +0300

    feat(serialzers): add feature for serializer
    menambahkan class serializer untuk semua model
    mungkin kedepannya akan ada perubahan untuk
    permission class

--- backend/kasir/serializers.py
+++ backend/kasir/serializers.py
@@ -6,3 +6,27 @@ class ProductSerializers(serializers.ModelSerializers):
     class Meta:
         model = Product
         fields = ["id", "name", "price", "stock", "category"]
+
+
+class CartSerializers(serializers.ModelSerializers):
+    class Meta:
+        model = Cart
+        fields = ["id", "amount", "product"]
+
+
+class CategorySerializers(serializers.ModelSerializers):
+    class Meta:
+        model = Category
+        fields = ["id", "name"]
+
+
+class TransactionySerializers(serializers.ModelSerializers):
+    class Meta:
+        model = Transaction
+        fields = ["id", "date", "amount"]
+
+
+class DetailTransactionSerializers(serializers.ModelSerializers):
+    class Meta:
+        model = DetailTransaction
+        fields = ["id", "transactrion", "product", "amount"]
