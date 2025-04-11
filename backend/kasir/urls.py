ddfabb105acd15af9e6930b57bbb4210c2ed4bba
Ndaru <daruprasetiyo123@gmail.com>
2025-04-11 16:48:26 +0700

    feat(url): add feature for urls
    menambahkan url untuk api product
    RetrieveUpdateDestroyAPIView

--- backend/kasir/urls.py
+++ backend/kasir/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("products/", views.ProductListView.as_view(), name="product-list"),
+    path("products/<int:product_id>", views.ProductRetrieveView.as_view(), name="product-retrieve"),
 ]
